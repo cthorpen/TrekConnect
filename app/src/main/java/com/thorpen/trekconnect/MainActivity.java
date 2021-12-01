e2fce9af7782b1a7b9476cedee583dd842269a1b
Cole Thorpen <thorpencole@gmail.com>
2021-12-01 14:55:02 -0800

    added dependency in gradle and permissions in manifest

--- app/src/main/java/com/thorpen/trekconnect/MainActivity.java
+++ app/src/main/java/com/thorpen/trekconnect/MainActivity.java
@@ -2,13 +2,39 @@ package com.thorpen.trekconnect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.os.Bundle;
 
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.connection.ConnectionsClient;
+import com.google.android.gms.nearby.connection.Strategy;
+
 public class MainActivity extends AppCompatActivity {
+
+    // Our handle to Nearby Connections
+    private ConnectionsClient connectionsClient;
+
+    private static final String TAG = "TrekConnect";
+
+    private static final String[] REQUIRED_PERMISSIONS =
+            new String[] {
+                    Manifest.permission.BLUETOOTH,
+                    Manifest.permission.BLUETOOTH_ADMIN,
+                    Manifest.permission.ACCESS_WIFI_STATE,
+                    Manifest.permission.CHANGE_WIFI_STATE,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+            };
+
+    private static final int REQUEST_CODE_REQUIRED_PERMISSIONS = 1;
+
+    private static final Strategy STRATEGY = Strategy.P2P_CLUSTER;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        connectionsClient = Nearby.getConnectionsClient(this);
     }
 }
