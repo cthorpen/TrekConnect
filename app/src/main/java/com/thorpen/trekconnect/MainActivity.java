4b8f53a1cb5c54a1771589052f3cec292607cf74
Cole Thorpen <thorpencole@gmail.com>
2021-12-01 18:04:13 -0800

    added navigation drawer

--- app/src/main/java/com/thorpen/trekconnect/MainActivity.java
+++ app/src/main/java/com/thorpen/trekconnect/MainActivity.java
@@ -1,9 +1,13 @@
 package com.thorpen.trekconnect;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.Manifest;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.gms.nearby.Nearby;
 import com.google.android.gms.nearby.connection.ConnectionsClient;
@@ -11,11 +15,10 @@ import com.google.android.gms.nearby.connection.Strategy;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Our handle to Nearby Connections
-    private ConnectionsClient connectionsClient;
-
     private static final String TAG = "TrekConnect";
 
+    // Our handle to Nearby Connections, and other Nearby Connections tools
+    private ConnectionsClient connectionsClient;
     private static final String[] REQUIRED_PERMISSIONS =
             new String[] {
                     Manifest.permission.BLUETOOTH,
@@ -24,17 +27,38 @@ public class MainActivity extends AppCompatActivity {
                     Manifest.permission.CHANGE_WIFI_STATE,
                     Manifest.permission.ACCESS_COARSE_LOCATION,
             };
-
     private static final int REQUEST_CODE_REQUIRED_PERMISSIONS = 1;
-
     private static final Strategy STRATEGY = Strategy.P2P_CLUSTER;
 
+    //Navigation Drawer tools
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /* for navigation drawer */
+        // drawer layout instance to toggle the menu icon to open drawer and back button to close
+        drawerLayout = findViewById(R.id.drawerLayout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+        //pass open and close toggle for the drawer layout listener to toggle button
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        //make the navigation drawer icon appear on the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //for Nearby Connections
         connectionsClient = Nearby.getConnectionsClient(this);
+    }
+
+    //override to implement the item click listener callback to open and close drawer when icon is clicked
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
