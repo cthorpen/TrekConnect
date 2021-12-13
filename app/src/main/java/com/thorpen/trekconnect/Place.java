4f9a0c90bc178ea9118d74b94a0458fdebd18277
R3TT4 <rettryu@gmail.com>
2021-12-13 15:10:19 -0800

    deleted testbutton, new map updates appropriately

---
+++ app/src/main/java/com/thorpen/trekconnect/Place.java
@@ -0,0 +1,52 @@
+package com.thorpen.trekconnect;
+
+public class Place {
+
+    // All the necessary field attributes for a single Place object
+    private String id;
+    private String name;
+    private String latitude;
+    private String longitude;
+
+    // Constructor for search
+
+
+    public Place(String id, String name, String latitude, String longitude) {
+        this.id = id;
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+}
