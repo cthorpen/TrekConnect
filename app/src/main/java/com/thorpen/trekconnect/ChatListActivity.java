0a468191a5f60e85b74690a1d19d9595fac87b1b
Cole Thorpen <thorpencole@gmail.com>
2021-12-13 14:54:21 -0800

    chat is good i think... for now

--- app/src/main/java/com/thorpen/trekconnect/ChatListActivity.java
+++ app/src/main/java/com/thorpen/trekconnect/ChatListActivity.java
@@ -3,22 +3,52 @@ package com.thorpen.trekconnect;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatListActivity extends AppCompatActivity {
 
     CustomAdapter adapter;
-    EditText editText;
+    Button sendButton;
+
+    // firebase fields
+    FirebaseDatabase mFirebaseDatabase;
+    // we are going to add an object called messages
+    DatabaseReference mMessagesDatabaseReference;
+    ChildEventListener mMessagesChildEventListener;
+
+    String userName = "Anonymous";
+
+    List<ChatMessage> chatMessageList;
+
+    String TAG = "ChatListActivityTag";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,30 +58,99 @@ public class ChatListActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
 
-        editText = findViewById(R.id.messageEditText);
-        editText.addTextChangedListener(new TextWatcher() {
+        chatMessageList = new ArrayList<>();
+
+        Intent intent = getIntent();
+
+        if (intent != null) {
+            userName = intent.getStringExtra("userName");
+        }
+
+
+        // handle button click actions
+        sendButton = findViewById(R.id.sendButton);
+        sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                // nothing
+            public void onClick(View v) {
+                Log.d(TAG, "sending message...");
+                EditText editText = (EditText) findViewById(R.id.editText);
+                String currText = editText.getText().toString();
+
+                if (currText.isEmpty()) {
+                    Toast.makeText(ChatListActivity.this,
+                            "enter a message before sending", Toast.LENGTH_SHORT).show();
+                } else {
+                    ChatMessage chatMessage = new ChatMessage(userName, currText);
+                    mMessagesDatabaseReference.push().setValue(chatMessage);
+                    // warmup task #1
+                    editText.setText("");
+                    chatMessageList.add(chatMessage);
+                    adapter.notifyItemInserted(chatMessageList.size());
+                }
+
             }
 
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                //set message text
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                //set message text
-            }
         });
 
-        RecyclerView recyclerView = findViewById(R.id.messageRecyclerView);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(ChatListActivity.this);
+        layoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(layoutManager);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(dividerItemDecoration);
         adapter = new CustomAdapter();
         recyclerView.setAdapter(adapter);
 
+        setupFirebase();
+    }
+
+    private void setupFirebase() {
+        // initialize the firebase references
+        FirebaseApp.initializeApp(this);
+        mFirebaseDatabase =
+                FirebaseDatabase.getInstance();
+        mMessagesDatabaseReference =
+                mFirebaseDatabase.getReference()
+                        .child("messages");
+        mMessagesChildEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                // called for each message already in our db
+                // called for each new message add to our db
+                // dataSnapshot stores the ChatMessage
+                Log.d(TAG, "onChildAdded: " + s);
+                ChatMessage chatMessage =
+                        dataSnapshot.getValue(ChatMessage.class);
+                // add it to our list and notify our adapter
+                chatMessageList.add(chatMessage);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
 
     }
+
+
+
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
@@ -70,11 +169,11 @@ public class ChatListActivity extends AppCompatActivity {
 
             public CustomViewHolder(@NonNull View itemView) {
                 super(itemView);
-//                text1 = itemView.findViewById(android.R.id.text1);
+                text1 = itemView.findViewById(android.R.id.text1);
             }
 
             public void updateView(ChatMessage c) {
-//                text1.setText(c.toString());
+                text1.setText(c.toString());
             }
         }
 
@@ -88,12 +187,12 @@ public class ChatListActivity extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(@NonNull CustomViewHolder holder, int position) {
-//            holder.updateView(chatMessageList.get(position));
+            holder.updateView(chatMessageList.get(position));
         }
 
         @Override
         public int getItemCount() {
-            return 0;
+            return chatMessageList.size();
         }
     }
 
--- app/src/main/java/com/thorpen/trekconnect/MainActivity.java
+++ app/src/main/java/com/thorpen/trekconnect/MainActivity.java
@@ -1,5 +1,9 @@
 package com.thorpen.trekconnect;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,26 +12,52 @@ import androidx.core.app.NotificationManagerCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Activity;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = "TrekConnect";
 
+    ActivityResultLauncher<Intent> launcher;
+
     //firebase account
     String userName = "Anonymous";
+    TextView welcomeTextView;
+
+    // firebase fields
+    FirebaseDatabase mFirebaseDatabase;
+    // we are going to add an object called messages
+    DatabaseReference mMessagesDatabaseReference;
+    ChildEventListener mMessagesChildEventListener;
+    // firebase authentication fields
+    FirebaseAuth mFirebaseAuth;
+    FirebaseAuth.AuthStateListener mAuthStateListener;
 
     //Navigation Drawer tools
     DrawerLayout drawerLayout;
@@ -47,6 +77,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setNavigationViewListener();
+
+        welcomeTextView = findViewById(R.id.welcomeTextView);
 
         /* for navigation drawer */
         // drawer layout instance to toggle the menu icon to open drawer and back button to close
@@ -79,7 +111,149 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         });
 
 
+        launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Toast.makeText(MainActivity.this, "You are now signed in", Toast.LENGTH_SHORT).show();
+                        }
+                        else if (result.getResultCode() == Activity.RESULT_CANCELED) {
+                            // they backed out of the sign in activity
+                            // let's exit
+                            finish();
+                        }
+                    }
+                });
+        setupFirebase();
     }
+
+    private void setupFirebase() {
+        // initialize the firebase references
+        FirebaseApp.initializeApp(this);
+        mFirebaseDatabase =
+                FirebaseDatabase.getInstance();
+        mMessagesDatabaseReference =
+                mFirebaseDatabase.getReference()
+                        .child("messages");
+        mMessagesChildEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                // called for each message already in our db
+                // called for each new message add to our db
+                // dataSnapshot stores the ChatMessage
+                Log.d(TAG, "onChildAdded: " + s);
+                ChatMessage chatMessage =
+                        dataSnapshot.getValue(ChatMessage.class);
+                // add it to our list and notify our adapter
+                //chatMessageList.add(chatMessage);
+                //adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+
+
+        // server side setup
+        // 1. enable authentication providers like
+        // email or google or facebook etc.
+        // today we will do email and google
+        // 2. return the default values for db
+        // read and write to be authenticated
+        // client side setup
+        // 3. declare a FirebaseAuth.AuthStateListener
+        // listens for authentication events
+        // signed in and signed out are our two states
+        // 4. if the user is signed in...
+        // let's get their user name
+        // wire up our childeventlistener mMessagesChildEventListener
+        // 5. if the user is not signed in...
+        // start an activity using FirebaseUI to
+        // log our user in
+        // 6. wire up the AuthStateListener in onResume()
+        // and detach it onPause()
+        // 7. add support for the user logging out
+        // with an options menu action
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                // we have two auth states, signed in and signed out
+                // get the get current user, if there is one
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // user is signed in
+                    // step 4
+                    setupUserSignedIn(user);
+                } else {
+                    // user is signed out
+                    // step 5
+                    // we need an intent
+                    // the firebaseUI Github repo README.md
+                    // we have used builders before in this class
+                    // AlertDialog.Builder
+                    // return instance to support chaining
+                    Intent intent = AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setIsSmartLockEnabled(false)
+                            .setAvailableProviders(
+                                    Arrays.asList(
+                                            new AuthUI.IdpConfig.EmailBuilder().build(),
+                                            new AuthUI.IdpConfig.GoogleBuilder().build()
+                                    )
+                            ).build();
+                    launcher.launch(intent);
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // attach the authstatelistener
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // remove it
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+//        chatMessageList.clear();
+    }
+
+    private void setupUserSignedIn(FirebaseUser user) {
+        // get the user's name
+        userName = user.getDisplayName();
+        // listen for database changes with childeventlistener
+        // wire it up!
+        mMessagesDatabaseReference
+                .addChildEventListener(mMessagesChildEventListener);
+
+        welcomeTextView.setText("Welcome, " + userName + "!");
+    }
+
+
 
     //override to implement the item click listener callback to open and close drawer when icon is clicked
     @Override
@@ -127,21 +301,20 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             case R.id.nav_account:
                 Toast.makeText(this, "nav account", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.nav_settings:
-                Toast.makeText(this, "nav settings", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.nav_findTrekkers:
-                Toast.makeText(this, "nav find trekkers", Toast.LENGTH_SHORT).show();
+            case R.id.nav_find_nearby_trails:
+                Toast.makeText(this, "nav find trails", Toast.LENGTH_SHORT).show();
                 intent = new Intent(MainActivity.this, MapsActivity.class);
                 startActivity(intent);
                 break;
             case R.id.nav_chat:
                 Toast.makeText(this, "nav chat", Toast.LENGTH_SHORT).show();
                 intent = new Intent(MainActivity.this, ChatListActivity.class);
-                startActivity(intent);
+                startChatListActivity(intent);
                 break;
             case R.id.nav_signout:
                 Toast.makeText(this, "nav sign out", Toast.LENGTH_SHORT).show();
+                AuthUI.getInstance().signOut(this);
+                mMessagesDatabaseReference.removeEventListener(mMessagesChildEventListener);
                 break;
         }
         drawerLayout.closeDrawer(GravityCompat.START);
@@ -153,6 +326,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         navigationView.setNavigationItemSelectedListener(this);
     }
 
+    public void startChatListActivity(Intent intent) {
+        intent.putExtra("userName", userName);
+        startActivity(intent);
+    }
 
 
 }
